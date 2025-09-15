ac2d99c1e46606b7ce396184eff4abe4e4189643
GuasiMorda <guasimorda@gmail.com>
2025-09-15 15:44:47 +0200

    2 gotovo

--- practic_programming/practic_two_second.py
+++ practic_programming/practic_two_second.py
@@ -4,7 +4,5 @@ user_input_choice = int(input("Выберите действие: "))
 user_input_number = int(input("Введите значение фигуры: "))
 if user_input_choice == 1:
     print("Радиус круга: ", user_input_number / 2)
-elif user_input_choice == 2:
-    print("Площадь квадрата: ", user_input_number * user_input_number)
 else:
-    print("Других выборов нету!")
+    print("Площадь квадрата: ", user_input_number * user_input_number)
